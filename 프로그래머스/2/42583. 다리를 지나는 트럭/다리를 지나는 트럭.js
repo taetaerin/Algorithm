c941ab4b8a778a05e18ac142e158215f47902de6
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-14 23:21:05 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 2.87 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,24 @@
+function solution(bridge_length, weight, truck_weights) {
+    let answer = 0;
+    
+    let que = new Array(bridge_length).fill(0);
+    
+    while(que.length) {
+        que.shift()
+        
+        if(truck_weights.length) {
+            let sum = que.reduce((a,b) => a+b)
+            if(weight < sum + truck_weights[0]) {
+                que.push(0)
+            }else{
+                let a =truck_weights.shift()
+                que.push(a)
+            }
+        }
+        
+        answer+=1
+        
+    }
+    console.log(que)
+    return answer
+}
