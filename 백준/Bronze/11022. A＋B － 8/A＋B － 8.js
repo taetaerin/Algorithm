8db02299d4c32f53dd001bcd54e28c6ad7260ceb
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-15 17:21:56 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+let a = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+const max = a[0]
+
+for(let i=1; i<= max; i++) {
+  let b = a[i].split(' ').map(Number)
+  console.log(`Case #${i}: ${b[0]} + ${b[1]} = ${b[0]+b[1]}`)
+}
