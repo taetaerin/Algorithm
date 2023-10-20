b66cc3b4f5c36d93f0764dc92fe052257a2423b1
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-21 00:21:05 +0900

    [Silver I] Title: 트럭, Time: 180 ms, Memory: 9636 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let [trucks_cnt, bridge_length, weight] = input[0].split(' ').map(Number);
+let trucks = input[1].split(' ').map(Number);
+
+let cnt = 0;
+let que = new Array(bridge_length).fill(0);
+let totalWeight = 0;
+
+while (que.length) {
+    cnt++;
+
+    // 큐에서 나가는 트럭의 무게를 빼줍니다.
+    totalWeight -= que.shift();
+    
+    if (trucks.length) {
+        // 새로운 트럭이 다리에 올라갈 수 있는 경우
+        if (totalWeight + trucks[0] <= weight) {
+            let newTruck = trucks.shift();
+            que.push(newTruck);
+            totalWeight += newTruck;
+        } else {
+            // 새로운 트럭이 다리에 올라갈 수 없는 경우
+            que.push(0);
+        }
+    }
+}
+
+console.log(cnt);
