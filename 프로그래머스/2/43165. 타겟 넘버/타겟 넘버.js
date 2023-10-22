e414765eecb7a0403b4c3fd84e5f0f80ee69e54b
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-22 22:13:03 +0900

    [level 2] Title: 타겟 넘버, Time: 1.10 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -2,15 +2,14 @@ function solution(numbers, target) {
     var answer = 0;
     
     function DFS(L, sum) {
-        if(L == numbers.length){
-            if(sum == target) {
-                answer += 1
-            }
+        if(L==numbers.length) {
+            if(sum == target) return answer += 1
+            
         }else{
             DFS(L+1, sum+numbers[L])
             DFS(L+1, sum-numbers[L])
         }
     }
-    DFS(0,0)
+    DFS(0, 0)
     return answer;
 }
