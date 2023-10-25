2828cc87e85eb3385c81d8b76805ac70c0265295
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-25 22:18:46 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,13 +1,10 @@
 function solution(array, commands) {
     var answer = [];
-    
-    for(let i=0; i<commands.length; i++) {
-        let [a, b, c] = commands[i]
-        let newL = array.slice(a-1, b)
-        newL.sort((a,b) => a-b)
-        answer.push(newL[c-1])
-
+    for(let x of commands) {
+        let [a, b, c] = x
+        let list =[]
+        list =array.slice(a-1, b).sort((a,b) => a-b)
+        answer.push(list[c-1])
     }
-    
     return answer;
 }
