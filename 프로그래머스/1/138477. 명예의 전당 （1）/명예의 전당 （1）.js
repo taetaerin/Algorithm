1c07053fae8ba794f021cb4f63eb24db56304951
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-25 21:42:59 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,13 @@
+function solution(k, score) {
+    var answer = [];
+    let list=[];
+    for(let i=0; i<score.length; i++) {
+        list.push(score[i])
+        list.sort((a,b) => a-b)
+        if(list.length > k) {
+            list.shift()
+        }
+        answer.push(list[0])
+    }
+    return answer;
+}
