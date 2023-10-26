1d46079997ce354d91a538b70d8be044f697fb35
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-26 16:54:57 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,14 @@
 function solution(nums) {
     var answer = 0;
-    const a = Math.floor(nums.length / 2)
-    console.log(a)
-    
+    let limit = nums.length/2
+
     nums = [...new Set(nums)]
-    console.log(nums)
-    if(nums.length > a) answer = a
-    else answer = nums.length
+
+    if(nums.length > limit) {
+        answer = limit
+    }else {
+        answer = nums.length
+    }
+
     return answer;
 }
