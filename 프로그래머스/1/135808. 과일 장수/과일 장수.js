0950d1069b58b59b8999fdadaa87d0e80d9be1d5
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-26 17:06:50 +0900

    [level 1] Title: 과일 장수, Time: 3.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,19 +1,13 @@
 function solution(k, m, score) {
     var answer = 0;
-    let a = Math.floor(score.length/m)
-    console.log('a', a)
-    
+    let a = parseInt(score.length/m)
     score.sort((a,b) => b-a)
-    console.log('score', score)
-    
+    console.log(score)
+
     for(let i=0; i<a; i++) {
-        let list = []
-        list.push(score.slice((m*i), (m*i)+m))
-  
-        answer += (list[0][m-1] * m)
-  
-        
+ 
+        let list = score.slice(m*i, (m*i)+m)   
+        answer += m * list[list.length-1]
     }
-    
     return answer;
 }
