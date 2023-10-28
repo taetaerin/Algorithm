64025eb1570434244cc4254ba3c23b520b00e672
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-28 19:16:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,7 @@
 function solution(phone_number) {
     var answer = '';
-    let a = phone_number.slice(0, -4)
-    a = '*'.repeat(a.length)
-    answer = a+phone_number.slice(-4)
+    let first = phone_number.slice(0, -4)
+    first = '*'.repeat(first.length)
+    answer = first+phone_number.slice(-4)
     return answer;
 }
