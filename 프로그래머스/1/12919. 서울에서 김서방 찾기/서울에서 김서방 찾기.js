afa0352022e094e88fab16500a9f21ab57f11021
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-28 19:11:28 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 2.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,4 +1,6 @@
 function solution(seoul) {
-    var answer = `김서방은 ${seoul.indexOf('Kim')}에 있다`;    
+    let idx = seoul.indexOf('Kim')
+    console.log(idx)
+    const answer = `김서방은 ${idx}에 있다`
     return answer;
 }
