270ace81479fd77adf39a1478dcc9c5f6ab3e8d8
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-28 19:13:11 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,8 +1,13 @@
 function solution(absolutes, signs) {
     var answer = 0;
-    for(let i=0; i<signs.length; i++) {
-        if(signs[i] == true) answer += absolutes[i]
-        else answer -= absolutes[i]
+    
+    for(let i=0; i<absolutes.length; i++) {
+        if(signs[i] == true) {
+            answer += absolutes[i]
+        }
+        else {
+            answer -= absolutes[i]
+        }
     }
     return answer;
 }
