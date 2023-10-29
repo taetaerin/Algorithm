f231da6e39a2353dc63dcccfed3b72a5929bbd15
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-29 23:06:46 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 9.25 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,14 @@
 function solution(s) {
     var answer = [];
     let stack = [];
-    
     for(let i=0; i<s.length; i++) {
         if(!stack.includes(s[i])) {
-            stack.push(s[i])
             answer.push(-1)
         }
-        else{
+        else {
             answer.push(stack.length - stack.lastIndexOf(s[i]))
-            stack.push(s[i])
         }
+        stack.push(s[i])
     }
     return answer;
 }
