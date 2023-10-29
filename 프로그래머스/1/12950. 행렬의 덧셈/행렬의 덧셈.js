b257b97a0818d5b5a88c774df1c7e2a12d6fbb73
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-29 21:55:03 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.96 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,13 +1,12 @@
 function solution(arr1, arr2) {
-    var answer = []
+    var answer = [];
+    
     for(let i=0; i<arr1.length; i++) {
-        let list=[]
-       for(let k=0; k<arr1[0].length; k++) {
-            list.push(arr1[i][k] + arr2[i][k])
-       }
-        answer.push(list)
+        let result = []
+        for(let j=0; j<arr1[0].length; j++) {
+            result.push(arr1[i][j] + arr2[i][j])
+        }
+        answer.push(result)
     }
-
-
     return answer;
 }
