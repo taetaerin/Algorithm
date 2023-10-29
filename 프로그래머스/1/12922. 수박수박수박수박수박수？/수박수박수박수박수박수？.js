c453dc64dea5196ef16afb4b0bf2c9b25a6b029b
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-29 21:17:00 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,8 @@
 function solution(n) {
     var answer = '';
     for(let i=0; i<n; i++) {
-        answer += (i%2==0) ? '수' : '박'
+        if(i%2==0) answer += '수'
+        else answer += '박'
     }
     return answer;
 }
