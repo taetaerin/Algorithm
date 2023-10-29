02b6fdb8290807f6aef487643176a3ccc95780c0
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-29 22:58:18 +0900

    [level 1] Title: 최소직사각형, Time: 7.99 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,13 +1,14 @@
 function solution(sizes) {
     var answer = 0;
-    let maxnum =[];
-    let minnum = [];
-    sizes = sizes.map(item => item.sort((a,b) => b-a))
+    let a = [];
+    let b = [];
+    
     for(let x of sizes) {
-        maxnum.push(x[0])
-        minnum.push(x[1])
+        x.sort((a,b) => b-a)
+        a.push(x[0])
+        b.push(x[1])
     }
-    console.log(maxnum, minnum)
-    answer = Math.max(...maxnum) * Math.max(...minnum)
+    
+    answer = Math.max(...a) * Math.max(...b)
     return answer;
 }
