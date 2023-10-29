f91b5eb6f73e2b1a1115b671eb50117c94423ef3
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-29 22:14:23 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 3.72 ms
+메모리: 33.5 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 16:21:11
+2023년 10월 0일 22:14:21
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,14 +1,13 @@
 function solution(d, budget) {
     var answer = 0;
-    let t = 0;
-    d = d.sort((a,b) => a-b)
-    console.log(d)
-
+    let total = 0;
+    d.sort((a,b) => a-b)
     for(let i=0; i<d.length; i++) {
-        t += d[i]
-        if(t <= budget) {
-            answer = i+1
+        total += d[i]
+        if(total <= budget) {
+            answer += 1
         }
     }
     return answer;
+    
 }
