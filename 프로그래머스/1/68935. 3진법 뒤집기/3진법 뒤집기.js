2e8399c407a1b4a908b4711af17b6ad587a10fbb
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-29 22:07:55 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     var answer = 0;
-    let a = (n.toString(3).split('').reverse().join(''))
-
+    let a = n.toString(3).split('').reverse().join('')
     answer = parseInt(a, 3)
+
     return answer;
 }
