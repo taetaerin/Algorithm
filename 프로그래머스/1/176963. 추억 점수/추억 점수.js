2422bbd35db0b96cd88bc3995ce239cd51340894
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-30 15:51:23 +0900

    [level 1] Title: 추억 점수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,16 +1,15 @@
 function solution(name, yearning, photo) {
     var answer = [];
-    
     for(let i=0; i<photo.length; i++) {
-        let hap =0;
+        let result = 0
         for(let j=0; j<photo[i].length; j++) {
             for(let k=0; k<name.length; k++) {
-                if(name[k] == photo[i][j]) {
-                    hap += yearning[k]
+                if(photo[i][j] == name[k]) {
+                    result += yearning[k]
                 }
             }
         }
-        answer.push(hap)
+        answer.push(result)
     }
     return answer;
 }
