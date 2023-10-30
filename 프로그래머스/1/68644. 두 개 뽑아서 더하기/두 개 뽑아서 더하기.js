c71ab5c26ef33aa51894961c99d8a9862eaa44ba
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-30 15:34:15 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.49 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,10 +1,13 @@
 function solution(numbers) {
     var answer = [];
+    let list = []
     for(let i=0; i<numbers.length; i++) {
         for(let j=i+1; j<numbers.length; j++) {
-            answer.push(numbers[i]+numbers[j])
-        }
+            list.push(numbers[i] + numbers[j])
+        } 
     }
-    answer = [...new Set(answer)].sort((a,b) => a-b)
+    
+    answer = [...new Set(list)].sort((a,b) => a-b)
+    
     return answer;
 }
