5c964a0e124a3d09f842420256d337f3215631c1
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-30 16:56:23 +0900

    [level 1] Title: 소수 만들기, Time: 3.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,29 +1,29 @@
 function solution(nums) {
     var answer = 0;
-    let list = []
-    
+    let list =[]
     for(let i=0; i<nums.length; i++) {
-        let hap = 0
         for(let j=i+1; j<nums.length; j++) {
             for(let k=j+1; k<nums.length; k++) {
-                hap = nums[i]+nums[j]+nums[k]
-                list.push(hap)
+                list.push(nums[i] + nums[j] + nums[k])
             }
         }
     }
     
-    for(let x of list) {
-        let cnt = 0;
-        for(let i=1; i<=Math.sqrt(x); i++) {
-            if(x%i == 0) {
-                cnt +=2
-                if(x%i == i) {
-                    cnt -=1
+    console.log(list)
+    
+    for(let i=0; i<list.length; i++) {
+        let a = 0
+        for(let k=1; k<=Math.sqrt(list[i]); k++) {
+            if(list[i] % k == 0) {
+                a += 2
+                if(list[i] / k == k) {
+                    a -= 1
                 }
             }
         }
-        if(cnt == 2) answer += 1   
+        if(a == 2) {
+            answer += 1
+        }
     }
-        
     return answer;
 }
