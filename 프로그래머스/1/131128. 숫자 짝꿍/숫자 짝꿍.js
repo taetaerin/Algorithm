ab5cd83d23ebf0652fb9d6a2abc3779760bb25bb
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-31 15:40:41 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -2,19 +2,38 @@ function solution(X, Y) {
     var answer = '';
     X = X.split('')
     Y = Y.split('')
-    let list = []
-    console.log(X,Y)
-
+    
     for(let i=0; i<=9; i++) {
-        let cur1 = X.filter((n) => Number(n) == i).length
-        let cur2 = Y.filter((n) => Number(n) == i).length
-        answer += String(i).repeat(Math.min(cur1, cur2))
-        
+        let arr1 = X.filter((item) => Number(item) === i).length;
+        let arr2 = Y.filter((item) => Number(item) === i).length;
+        answer += String(i).repeat(Math.min(arr1, arr2))
     }
     
     if(answer.length == 0) return '-1'
     if(Number(answer) == 0) return '0'
-    answer = answer.split('').reverse().join('')
+    
+    answer = answer.split('').sort((a,b)=>b-a).join('')
     
     return answer;
 }
+
+
+
+
+
+
+//     X = X.split('')
+//     Y = Y.split('')
+//     let list = []
+//     console.log(X,Y)
+
+//     for(let i=0; i<=9; i++) {
+//         let cur1 = X.filter((n) => Number(n) == i).length
+//         let cur2 = Y.filter((n) => Number(n) == i).length
+//         answer += String(i).repeat(Math.min(cur1, cur2))
+        
+//     }
+    
+//     if(answer.length == 0) return '-1'
+//     if(Number(answer) == 0) return '0'
+//     answer = answer.split('').reverse().join('')
