d1c34f39a4b0c30bf916f2e306c90b785d587a3c
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-31 21:24:31 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,15 +1,32 @@
 function solution(n, m) {
     var answer = [];
-    let minNum = 0;
-
+    let num = 0;
+     
     for(let i=1; i<=m; i++) {
         if(n%i == 0 && m%i == 0) {
-            minNum = i
+            num = i
         }
     }
-    
-    let maxNum = (n*m) / minNum
-    
-    answer.push(minNum, maxNum)
+
+    answer.push(num, (n*m/num))
     return answer;
 }
+
+
+
+
+
+
+
+
+//     let minNum = 0;
+
+//     for(let i=1; i<=m; i++) {
+//         if(n%i == 0 && m%i == 0) {
+//             minNum = i
+//         }
+//     }
+    
+//     let maxNum = (n*m) / minNum
+    
+//     answer.push(minNum, maxNum)
