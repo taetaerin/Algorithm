c512fb328e67f7b9584e3973c5bdba2a710741bc
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-10-31 20:40:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,20 @@
 function solution(num) {
     var answer = 0;
+    let cnt = 0;
     
-    while(num != 1) {
-        if(num%2==0) {
-            num /= 2
-        }else {
-            num = num*3+1
+    while(num !== 1) {
+        if(num%2 == 0) {
+            num = num / 2
+        }else{
+            num = num * 3 + 1
         }
-        answer++
+        cnt += 1
     }
     
-    if(answer > 500) return -1
-    
+    if(cnt > 500) {
+        return -1
+    }else{
+        answer = cnt
+    }
     return answer;
 }
