884c2768a4095c56da47e9435ce965a3e01bc0f0
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-11-01 20:16:54 +0900

    [level unrated] Title: A로 B 만들기, Time: 3.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,14 @@
+function solution(before, after) {
+    var answer = 1;
+    
+    before = before.split('').sort()
+    after = after.split('').sort()
+    console.log(before, after)
+    
+    for(let i=0; i<before.length; i++) {
+        if(before[i] !== after[i]) {
+            return 0
+        }
+    }
+    return answer;
+}
