e74c4edae70c5bac4d826769ab0a7b8d22ae059e
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-11-02 18:47:25 +0900

    [level unrated] Title: 문자열 밀기, Time: 3.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
@@ -1,13 +1,29 @@
 function solution(A, B) {
-    var answer = 0;
-    if(A==B) return 0
-    A = A.split('')
-    for(let i=0; i<A.length; i++) {   
-        let a = A.pop()
-        A.unshift(a)
-        if(A.join('') == B) {
+    var answer = -1;
+    let list = A.split('')
+    if(A == B) return 0
+   
+    for(let i=0; i<A.length; i++) {
+        let word = list.pop()
+        list.unshift(word)
+        console.log(list)
+        if(list.join('') == B) {
             return i+1
         }
     }
-    return -1
+    return answer
 }
+
+
+
+
+
+ // if(A==B) return 0
+ //    A = A.split('')
+ //    for(let i=0; i<A.length; i++) {   
+ //        let a = A.pop()
+ //        A.unshift(a)
+ //        if(A.join('') == B) {
+ //            return i+1
+ //        }
+ //    }
