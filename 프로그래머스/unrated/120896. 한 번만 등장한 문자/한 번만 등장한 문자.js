f76ba392937c27647a3557ab67a752df503be6f3
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-11-02 16:08:47 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 2.83 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,17 +1,37 @@
 function solution(s) {
-    var answer = '';
-    let listSet = [...new Set(s)]
+    var answer = [];
     s = s.split('')
-    console.log(listSet)
+    const word = [...new Set(s)]
     
-    for(let i=0; i<listSet.length; i++) {
-        let result = s.filter(item => item == listSet[i]).length
-        console.log(result)
-        
-        if(result == 1) {
-            answer += listSet[i]
+    for(let i=0; i<word.length; i++) {
+        let a = s.filter(item => item == word[i]).length
+        console.log(a)
+        if(a == 1) {
+            answer.push(word[i])
         }
     }
-    answer = answer.split('').sort().join('')
+    
+    answer = answer.sort().join('')
     return answer;
 }
+
+
+
+
+
+
+
+
+//  let listSet = [...new Set(s)]
+//     s = s.split('')
+//     console.log(listSet)
+    
+//     for(let i=0; i<listSet.length; i++) {
+//         let result = s.filter(item => item == listSet[i]).length
+//         console.log(result)
+        
+//         if(result == 1) {
+//             answer += listSet[i]
+//         }
+//     }
+//     answer = answer.split('').sort().join('')
