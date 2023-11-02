e360dfc28bf77de1931f0dd22008ee45977fc49e
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-11-02 20:24:55 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 3.80 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,25 +1,54 @@
 function solution(lines) {
     var answer = 0;
-    let minnum = 123456
-    let maxnum = -123456
+
+    let word = lines.join(',').split(',')
+    let min = Math.min(...word)
+    let max = Math.max(...word)
+    let list = new Array(max-min+1).fill(0)
+    
+//     console.log(min, max)
+//     console.log(list)
     
     for(let x of lines) {
-        minnum = Math.min(x[0], minnum)
-        maxnum = Math.max(x[1], maxnum)
+        console.log(x)
+        for(let i=x[0]; i<x[1]; i++) {
+            list[i-min] += 1
+        }
+    }
+    
+     console.log(list)
+    
+    for(let x of list) {
+        if(x>=2) answer +=1
     }
 
-    let list = new Array(maxnum-minnum+1).fill(0)
-    
-    for(let x of lines) {
-        for(let i=x[0]; i<x[1]; i++) {
-            list[i-minnum] += 1
-        }
-    }
-    console.log(list)
-    for(let x of list) {
-        if(x > 1) {
-            answer += 1
-        }
-    }
     return answer;
 }
+
+
+
+
+
+
+//     let minnum = 123456
+//     let maxnum = -123456
+    
+//     for(let x of lines) {
+//         minnum = Math.min(x[0], minnum)
+//         maxnum = Math.max(x[1], maxnum)
+//     }
+
+//     let list = new Array(maxnum-minnum+1).fill(0)
+    
+//     for(let x of lines) {
+//         for(let i=x[0]; i<x[1]; i++) {
+//             list[i-minnum] += 1
+//              console.log(list)
+//         }
+//     }
+   
+//     for(let x of list) {
+//         if(x > 1) {
+//             answer += 1
+//         }
+//     }
