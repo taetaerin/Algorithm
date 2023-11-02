321eb4abaf40935b4575aa8a0215f9b137acae0c
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-11-02 18:38:20 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 13.07 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,14 +1,36 @@
 function solution(n) {
     var answer = 0;
     let list = []
+    let list2 = []
     
-    for(let i=1; i<=100000; i++) {
+    for(let i=1; i<= 10000; i++) {
         list.push(i)
     }
     
-    list = list.filter((item) => !(String(item).includes('3') || item%3==0))
-    console.log(list)
+    let a = list.filter(item => item%3 !==0)
+    for(let x of a) {
+        if(!String(x).includes('3')) {
+            list2.push(x)
+        }
+    }
+    console.log(list2)
     
-    answer = list[n-1]
+    answer = list2[n-1]
     return answer;
 }
+
+
+
+
+
+
+//     let list = []
+    
+//     for(let i=1; i<=100000; i++) {
+//         list.push(i)
+//     }
+    
+//     list = list.filter((item) => !(String(item).includes('3') || item%3==0))
+//     console.log(list)
+    
+//     answer = list[n-1]
