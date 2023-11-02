30d1565a5018048e4c33e82ef3218828c12dc1a8
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-11-02 16:13:09 +0900

    [level unrated] Title: 컨트롤 제트, Time: 3.76 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/unrated/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,20 +1,41 @@
 function solution(s) {
     var answer = 0;
-    let stack = []
+    let stack = [];
     
     s = s.split(' ')
-    for(let i=0; i<s.length; i++) {
-        if(s[i] == 'Z') {
+    console.log(s)
+    
+    for(let x of s) {
+        if(x == 'Z') {
             stack.pop()
         }else {
-            stack.push(Number(s[i]))
-
+            stack.push(Number(x))
         }
     }
-              
+    console.log(stack)
+    
     if(stack.length !== 0) {
         answer = stack.reduce((a,b) => a+b)
     }
     
     return answer;
 }
+
+
+
+// let stack = []
+    
+//     s = s.split(' ')
+//     for(let i=0; i<s.length; i++) {
+//         if(s[i] == 'Z') {
+//             stack.pop()
+//         }else {
+//             stack.push(Number(s[i]))
+
+//         }
+//     }
+              
+//     if(stack.length !== 0) {
+//         answer = stack.reduce((a,b) => a+b)
+//     }
+    
