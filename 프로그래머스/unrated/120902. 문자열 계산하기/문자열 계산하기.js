81363c28a1141588c356aef396477c77cd460756
taetaerin <86650546+taetaerin@users.noreply.github.com>
2023-11-02 16:51:11 +0900

    [level unrated] Title: 문자열 계산하기, Time: 3.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,18 +1,38 @@
 function solution(my_string) {
-
-    let a = my_string.split(' ')
-    var answer = Number(a[0]);
     
-    for(let i=0; i<a.length; i++) {
-   
-        if(a[i] == '+') {
-            answer += Number(a[i+1])
-            console.log(answer)
+    let word = my_string.split(' ')
+    console.log(word)
+
+    let answer = Number(word[0]);
+    console.log(answer)
+
+    for(let i=1; i<word.length; i++) {
+        if(word[i] == '+') {
+            answer += Number(word[i+1])
         }
-        if(a[i] == '-') {
-            answer -=  Number(a[i+1])
-            console.log(answer)
+        if(word[i] == '-') {
+            answer -= Number(word[i+1])
         }
     }
+    
+    console.log(answer)
+
     return answer;
 }
+
+
+
+//     let a = my_string.split(' ')
+//     var answer = Number(a[0]);
+    
+//     for(let i=0; i<a.length; i++) {
+   
+//         if(a[i] == '+') {
+//             answer += Number(a[i+1])
+//             console.log(answer)
+//         }
+//         if(a[i] == '-') {
+//             answer -=  Number(a[i+1])
+//             console.log(answer)
+//         }
+//     }
