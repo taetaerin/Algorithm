da190a032f3c08e663c9fec678eb4a1757cadb83
taerin <86650546+taetaerin@users.noreply.github.com>
2023-11-24 17:03:31 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,33 +1,31 @@
+function solution(n) {
+    var answer = 0;
+    
+    const one_num = n.toString(2).split('').filter(item => item == 1).length;
+
+    for(let i=n+1; i<1000000; i++) {
+        let result = i.toString(2).split('').filter(item => item == 1).length;
+        if(result == one_num) {
+            answer = i
+            break;
+        }
+    }
+    return answer;
+}
+
 // function solution(n) {
 //     var answer = 0;
     
 //     const one_num = n.toString(2).split('').filter(item => item == 1).length;
     
-//     console.log(one_num)
-    
-//     for(let i=n+1; i<10000; i++) {
-//         let result = i.toString(2).split('').filter(item => item == 1).length;
-//         if(result == one_num) {
-//             answer = i
+//     let result = n+1
+//     while(result > n) {
+//         let a = result.toString(2).split('').filter(item => item == 1).length;
+//         if(a == one_num) {
+//             answer = result
 //             break;
 //         }
+//         result += 1
 //     }
 //     return answer;
 // }
-
-function solution(n) {
-    var answer = 0;
-    
-    const one_num = n.toString(2).split('').filter(item => item == 1).length;
-    
-    let result = n+1
-    while(result > n) {
-        let a = result.toString(2).split('').filter(item => item == 1).length;
-        if(a == one_num) {
-            answer = result
-            break;
-        }
-        result += 1
-    }
-    return answer;
-}
