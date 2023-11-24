3ac1c1dafaac9d9551eab103f7f7d5e7503e91bd
taerin <86650546+taetaerin@users.noreply.github.com>
2023-11-24 16:36:58 +0900

    [level 2] Title: 숫자의 표현, Time: 1.47 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,12 +1,45 @@
+// function solution(n) {
+//     var answer = 0;
+//     let list = Array.from({length: n}, (v, i) =>i+1)
+    
+//     let left = 0;
+
+    
+//     for(let right=0; right<n; right++) {
+//         sum += list[right];
+        
+//         while(sum > n) {
+//             sum -= list[left]
+//             left += 1
+//         }
+        
+//         if(sum == n) {
+//             answer += 1
+//         }
+        
+//     }
+//     return answer;
+// }
+
 function solution(n) {
     var answer = 0;
-    let hap = 0
-    for(let i=1; i<=n; i++) {
-        hap += i-1
-        if(hap<n && (n-hap)%i == 0) {
-            console.log(i, hap)
+    
+    let sum = 0;
+    let left = 1;
+
+    
+    for(let right=1; right<=n; right++) {
+        sum += right;
+        
+        while(sum > n) {
+            sum -= left
+            left += 1
+        }
+        
+        if(sum == n) {
             answer += 1
         }
+        
     }
     return answer;
 }
