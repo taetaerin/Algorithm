53fce923e672d25eab27f5760aff7bb002361da7
taerin <86650546+taetaerin@users.noreply.github.com>
2023-12-01 17:07:49 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n){
+    var ans = 0;
+    
+    while(n>0) {
+        if(n%2 !==0) {
+            ans +=1
+            n -=1
+        }else{
+            n = n/2
+        }
+    }
+
+    return ans;
+}
