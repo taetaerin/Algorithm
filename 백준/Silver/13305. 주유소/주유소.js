92810fa8893d1be00005283850fbe23a7c37084d
taerin <86650546+taetaerin@users.noreply.github.com>
2023-12-03 18:44:18 +0900

    [Silver III] Title: 주유소, Time: 224 ms, Memory: 19916 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let n = Number(input[0])
+
+let distance = input[1].split(' ').map(Number)
+let price = input[2].split(' ').map(Number)
+
+let min = price[0]
+let answer = 0;
+for(let i=0; i<price.length-1; i++) {
+  if(min >= price[i]) {
+    min = price[i]
+  }
+  answer += min * distance[i]
+}
+
+console.log(answer)
