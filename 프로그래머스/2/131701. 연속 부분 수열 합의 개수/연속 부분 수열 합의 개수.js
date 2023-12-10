f43304b957d00614f24f38f6570ff9d92a7384e6
taerin <86650546+taetaerin@users.noreply.github.com>
2023-12-10 21:53:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1024.53 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,20 +1,16 @@
 function solution(elements) {
     var answer = 0;
     let list = []
-    //elements.sort((a,b) => a-b)
     
     for(let n=1; n<=elements.length; n++) {
-        
         for(let i=0; i<elements.length; i++) {
-            let newList = elements.slice(0, n)
-            list.push(newList.reduce((a,b) => a+b))
+            let sum = elements.slice(0, n).reduce((a,b) => a+b)
+            list.push(sum)
             let first = elements.shift()
             elements.push(first)
-            
         }
-        
     }
-    
+
      answer = [...new Set(list)].length
     
     return answer;
