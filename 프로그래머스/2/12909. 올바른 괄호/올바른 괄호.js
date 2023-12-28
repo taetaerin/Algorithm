5ce2f355d5bc92ab1b2d92691c932d99e1930e19
taerin <86650546+taetaerin@users.noreply.github.com>
2023-12-28 19:16:42 +0900

    [level 2] Title: 올바른 괄호, Time: 10.25 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,22 +1,46 @@
 function solution(s){
-    var answer = false; 
+    var answer; 
     let stack = [];
     
-    for(let i=0; i<s.length; i++) {
-        if(s[i] == '(') {
-            stack.push(s[i])
-        }else{
-            if(stack.length !== 0) {
-                stack.pop() 
-            }else{
-                return false
+    for(let x of s) {
+        if(stack[stack.length -1] == '(') {
+            if(x == ')') {
+                stack.pop()
+            }else {
+                stack.push(x)
             }
         }
+        else {
+            stack.push(x)
+        }
+        //console.log(stack)
     }
     
-    if(stack.length == 0) {
-        answer = true
-    }
-    
+    answer = stack.length == 0 ? true : false
     return answer;
 }
+
+
+
+
+
+
+
+
+
+// for(let i=0; i<s.length; i++) {
+//         if(s[i] == '(') {
+//             stack.push(s[i])
+//         }else{
+//             if(stack.length !== 0) {
+//                 stack.pop() 
+//             }else{
+//                 return false
+//             }
+//         }
+//     }
+    
+//     if(stack.length == 0) {
+//         answer = true
+//     }
+    
