eb22a901e8fc7246a5dea54c253cdb137c0eb07e
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-03 22:48:23 +0900

    [Silver II] Title: 주식, Time: 2432 ms, Memory: 437604 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.js
+++ 백준/Silver/11501. 주식/주식.js
@@ -1,23 +1,26 @@
+const { constants } = require('buffer');
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
+let n = input.shift()
 
-let n = input.shift();
-let result = []
+let list = []
 
-for(let i=0; i<input.length; i+=2) {
-    const N = input[i]
-    let list = input[i+1].split(' ').map(Number)
-    let maxNum = 0;
-    let answer = 0;
+for(let i=1; i<input.length; i+=2) {
+    list.push(input[i].split(' ').map(Number))
+}
 
-    for(let i=N-1; i>=0; i--) {
+for(let x of list) {
+    let answer = 0
+    let num = x[x.length-1]
+    for(let i=x.length-1; i>=0; i--) {
+        if(x[i] < num) {
+            answer += num - x[i]
+        }else {
+            num = x[i]
+        }
 
-      if(list[i] > maxNum) maxNum = list[i]
-      else {
-        answer += maxNum - list[i]
-      }
     }
-  console.log(answer)
+    console.log(answer)
 }
