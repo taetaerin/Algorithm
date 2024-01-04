21f7c1079401cd8dba5e77e11857c317c08902e5
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-04 23:07:22 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -2,18 +2,23 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-input.shift()
 
-let dp = Array.from({length: 11}, () => 0) 
-dp[1] = 1;
-dp[2] = 2;
-dp[3] = 4;
+let n = input.shift()
+let list = input.map(Number)
+let maxNum = Math.max(...list)
 
+let dp = Array.from({length: maxNum+1}, () => 0)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
-for(let i=4; i<=10; i++) {
-    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+for(let i=4; i<= maxNum; i++) {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 }
 
-for(let x of input) {
-    console.log(dp[x])
+let answer = []
+for(let x of list) {
+    answer.push(dp[x])
 }
+
+console.log(answer.join('\n'))
