fdde723f5ac4933ab5d8ff4e12a3a0cff11e8601
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-04 21:51:14 +0900

    [Gold IV] Title: 수열의 점수, Time: 280 ms, Memory: 28220 KB -BaekjoonHub

--- 백준/Gold/2036. 수열의 점수/수열의 점수.js
+++ 백준/Gold/2036. 수열의 점수/수열의 점수.js
@@ -4,44 +4,44 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
 let n = input.shift()
 
-let answer = 0n;
+let plus = []
+let minus =[]
+let answer = 0n
 
-const plus = [];
-const minus = [];
-
-for (let x of input) {
-  if (x > 1) {
-    plus.push(x);
-  } else if (x <= 0) {
-    minus.push(x);
-  } else {
-    answer += 1n;
-  }
+for(let x of input) {
+    if(x > 1) {
+        plus.push(x)
+    }else if(x<=0) {
+        minus.push(x)
+    }else {
+        answer += 1n
+    }
 }
 
-plus.sort((a, b) => b-a);
-minus.sort((a,b) => a-b); 
+plus.sort((a,b) => b-a)
+minus.sort((a,b) => a-b)
 
-if (plus.length % 2 === 0) {
-  for (let i = 0; i < plus.length; i += 2) {
-    answer += BigInt(plus[i] * plus[i + 1]);
-  }
-} else {
-  for (let i = 0; i < plus.length - 1; i += 2) {
-    answer += BigInt(plus[i] * plus[i + 1]);
-  }
-  answer += BigInt(plus[plus.length - 1]);
+
+if(plus.length % 2 === 0) {
+    for(let i=0; i<plus.length; i+=2) {
+        answer +=  BigInt(plus[i] * plus[i+1])
+    }
+}else {
+    for(let i=0; i<plus.length-1; i+=2) {
+        answer +=  BigInt(plus[i] * plus[i+1])
+    }
+    answer += BigInt(plus[plus.length-1])
 }
 
-if (minus.length % 2 === 0) {
-  for (let i = 0; i < minus.length; i += 2) {
-    answer += BigInt(minus[i] * minus[i + 1]);
-  }
-} else {
-  for (let i = 0; i < minus.length - 1; i += 2) {
-    answer += BigInt(minus[i] * minus[i + 1]);
-  }
-  answer += BigInt(minus[minus.length - 1]);
-}
 
-console.log(answer.toString());
+if(minus.length % 2 === 0) {
+    for(let i=0; i<minus.length; i+=2) {
+        answer +=  BigInt(minus[i] * minus[i+1])
+    }
+}else {
+    for(let i=0; i<minus.length-1; i+=2) {
+        answer +=  BigInt(minus[i] * minus[i+1])
+    }
+    answer += BigInt(minus[minus.length-1])
+} 
+console.log(answer.toString())
