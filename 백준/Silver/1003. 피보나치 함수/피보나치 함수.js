2f4acf95346638c415d741e8f932191645c275e5
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-07 22:59:07 +0900

    [Silver III] Title: 피보나치 함수, Time: 128 ms, Memory: 9888 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -3,19 +3,19 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const n = input.shift()
+let n = input.shift()
+let list = input.map(Number)
+let maxNum = Math.max(...list)
 
-input = input.map(i => Number(i))
-const maxNum = Math.max(...input)
-
-let dp = Array.from({length: maxNum+1})
+let dp = Array.from({length: maxNum+1}, () => [0,0])
 dp[0] = [1, 0]
 dp[1] = [0, 1]
 
 for(let i=2; i<=maxNum; i++) {
-    dp[i] = [dp[i-2][0] +dp[i-1][0], dp[i-2][1] +dp[i-1][1]]
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
 }
 
-for(let x of input) {
-    console.log(dp[x].join(' '))
+for(let x of list) {
+    console.log(dp[x][0], dp[x][1])
 }
