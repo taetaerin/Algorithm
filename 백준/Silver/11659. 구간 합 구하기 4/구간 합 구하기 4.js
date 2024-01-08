da1b3556e10822d3606c4693159b3d2078eb5e06
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-08 21:56:59 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 404 ms, Memory: 48072 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -2,24 +2,24 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
 let [n, m] = input.shift().split(' ').map(Number)
+let list = input.shift()
 
-let list = input.shift().split(' ').map(Number)
+let board = list.split(' ').map(Number)
 
-let dp = Array.from({length: Number(n)+1} , () => 0)
+let dp = Array.from({length: Number(n)+1}, () => 0)
 dp[0] = 0
-dp[1] = list[0]
-
+dp[1] = board[0]
 
 for(let i=2; i<=n; i++) {
-    dp[i] = dp[i-1] + list[i-1]
+    dp[i] = board[i-1] + dp[i-1]
 }
 
-let result = []
-
+let answer = []
 for(let x of input) {
     let [start, end] = x.split(' ').map(Number)
-    result.push(dp[end] - dp[start-1])
+    answer.push(dp[end] - dp[start-1])
 }
 
-console.log(result.join('\n'))
+console.log(answer.join('\n'))
