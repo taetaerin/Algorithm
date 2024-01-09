c87283d837262a4923239e1ead314f8d7ac347c3
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-09 23:35:05 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 200 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -4,11 +4,12 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let n = Number(fs.readFileSync(filePath).toString().trim());
 
 let dp = Array.from({length: n+1}, () => 0)
-dp[0] = 1;
-dp[1] = 1;
+dp[0] = 0
+dp[1] = 1
+dp[2] = 3
 
-for(let i=2; i<=n; i++) {
-    dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007
+for(let i=3; i<=n; i++) {
+    dp[i] = (dp[i-1] + dp[i-2]*2) % 10007
 }
 
 console.log(dp[n])
