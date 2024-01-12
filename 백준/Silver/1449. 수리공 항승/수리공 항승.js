5d7dca68f36f5546768af4ab42c16e6fc83f04f6
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-12 14:51:10 +0900

    [Silver III] Title: 수리공 항승, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.js
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.js
@@ -1,19 +1,18 @@
-const fs = require('fs')
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-let [n, m] = input.shift().split(' ')
-
-let list = input[0].split(' ').sort((a,b) => a-b).map(Number)
-
-let end = list[0] + (m-1);
-let answer = 1;
+const [n,L] = input.shift().split(' ').map(Number)
+let list = input[0].split(' ').map(Number).sort((a,b) => a-b)
+let last = 0;
+let answer = 0;
 
 for(let x of list) {
-  if(x > end) {
-      answer += 1
-      end = x + (m-1)
-  }
+    if(x > last) {
+        last = x + L - 1;
+        answer += 1
+    }
 }
 
 console.log(answer)
