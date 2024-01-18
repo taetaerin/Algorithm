d0a2eda8f74ccf69e56fe1196fb4a356a62e0531
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-18 20:35:36 +0900

    [level 2] Title: 주식가격, Time: 7.38 ms, Memory: 42.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,14 @@
+function solution(prices) {
+    var answer = [];
+    for(let i=0; i<prices.length; i++) {
+        let count = 0
+        for(let j=i+1; j<prices.length; j++) {
+            count += 1
+            if(prices[i] > prices[j]) {
+                break
+            }
+        }
+        answer.push(count)
+    }
+    return answer;
+}
