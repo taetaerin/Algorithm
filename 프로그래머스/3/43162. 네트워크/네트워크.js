dc112c06c094f55a11074e784e7b160ad00447c4
taerin <86650546+taetaerin@users.noreply.github.com>
2024-01-26 14:28:42 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -2,23 +2,22 @@ function solution(n, computers) {
     var answer = 0;
     let visited = Array.from({length: n}, () => 0)
     
-    
-    function DFS(i) {
-        visited[i] = 1;
-        for(let j=0; j<n; j++) {
-            if(computers[i][j] == 1 && visited[j] == 0) {
-                DFS(j)
+    function DFS(k) {
+        visited[k] = 1
+
+        for(let i=0; i<n; i++){
+            if(computers[k][i] == 1 && visited[i] == 0) {
+                DFS(i)
             }
         }
-    }
+     }
     
     for(let i=0; i<n; i++) {
         if(visited[i] == 0) {
-        
             DFS(i)
             answer += 1
-        }
+        } 
+            
     }
-    
     return answer;
 }
